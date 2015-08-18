549279584a68556a0a3478ba9ec4132c405335fe
Paul Hendrick <paul@errorstudio.co.uk>
2015-08-18 15:56:36 +0100

    initial commit - boilerplate plugin

---
+++ includes/class-justified-admin-theme-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://errorstudio.co.uk
+ * @since      1.0.0
+ *
+ * @package    Justified_Admin_Theme
+ * @subpackage Justified_Admin_Theme/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Justified_Admin_Theme
+ * @subpackage Justified_Admin_Theme/includes
+ * @author     Error <info@errorstudio.co.uk>
+ */
+class Justified_Admin_Theme_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
