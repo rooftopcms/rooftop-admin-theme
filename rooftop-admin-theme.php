50c8825bb3f445d33738f2dd8578778f4bb83a0b
Paul Hendrick <paul@errorstudio.co.uk>
2015-08-27 10:41:24 +0100

    change wordpress plugin name

--- rooftop-admin-theme.php
+++ rooftop-admin-theme.php
@@ -13,7 +13,7 @@
  * @package           Rooftop_Admin_Theme
  *
  * @wordpress-plugin
- * Plugin Name:       Justified Admin Theme
+ * Plugin Name:       Rooftop Admin Theme
  * Plugin URI:        rooftop-admin-theme
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
